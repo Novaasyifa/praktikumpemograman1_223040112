d5eb121c48d569f0c105f12fd846f4d767f387a9
Novaasyifa <126181389+Novaasyifa@users.noreply.github.com>
2024-03-01 04:32:01 -0800

    Add files via upload

---
+++ tugas1/Node.java
@@ -0,0 +1,29 @@
+package tugas1;
+
+public class Node {
+	private int nilai;
+	private Node next;
+	
+	//inisialisasi Node
+	public Node(int nilai) {
+		this.nilai = nilai;
+		this.next = null;
+	}
+	
+	public void setNilai(int nilai) {
+		this.nilai = nilai;
+	}
+	public int getNilai() {
+		return nilai;
+	}
+	
+	//setter dan getter
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	public Node getNext() {
+		return next;
+	}
+	
+
+}
---
+++ tugas4b/StrukturListTest1.java
@@ -0,0 +1,33 @@
+package tugas4b;
+
+public class StrukturListTest1 {
+	
+	public static void main(String[] args) {
+        StrukturList myList = new StrukturList();
+
+     // Tambah elemen 2.1 di awal list
+        myList.addFirst(2.1);
+
+        // Tambah elemen 3.4 di akhir list
+        myList.addLast(3.4);
+
+        // Tambah elemen 4.5 di akhir list
+        myList.addLast(4.5);
+
+        // Tampilkan elemen list
+        displayList(myList);
+    }
+
+    // Metode untuk menampilkan elemen-elemen dari list
+    private static void displayList(tugas4b.StrukturList list) {
+        Node current = list.getHead();
+
+        while (current != null) {
+            System.out.print(current.getData() + " ");
+            current = current.getNext();
+        }
+
+        System.out.println();
+    }
+
+}
